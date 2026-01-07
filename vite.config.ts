d119d9fbf39c2058fce0eaca2e3b1dfa989b2f73
samuel00410 <samuel00410@gmail.com>
2026-01-07 23:44:13 +0800

    修改vite設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 設定 base 路徑，請將 'coffeeShopWebsite' 改成你的 GitHub repo 名稱
+  // 如果是部署到 username.github.io（主頁），則設為 '/'
+  base: "/coffeeShopWebsite/",
   plugins: [vue(), tailwindcss()],
 });
